988693d3ac706b873d8a3a6e6d2c50d0d16380d6
Tonero101 <tonyebuzz@gmail.com>
2017-04-06 20:40:42 -0500

    validation source

--- edu.utd.cse.cerbac.validation/src/edu/utd/cse/cerbac/validation/CerbacRulesConstraint.java
+++ edu.utd.cse.cerbac.validation/src/edu/utd/cse/cerbac/validation/CerbacRulesConstraint.java
@@ -5,6 +5,7 @@ import org.eclipse.emf.validation.AbstractModelConstraint;
 import org.eclipse.emf.validation.IValidationContext;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.util.UMLUtil;
 
 import edu.utd.cse.cerbac.cerbac.CerbacPolicy;
 
@@ -12,13 +13,19 @@ public class CerbacRulesConstraint extends AbstractModelConstraint {
 
 	@Override
 	public IStatus validate(IValidationContext context) {
-		
-		CerbacPolicy cp = (CerbacPolicy) context.getTarget();
-		
+		System.out.println("<<context::" + context);
 		
 		EObject target = context.getTarget();
+		System.out.println("<<Type of Target::" + target.getClass());
+		
+	
 		if (target instanceof Element) {
 			Element element = (Element) target;
+			if (UMLUtil.getStereotypeApplication(element, CerbacPolicy.class) != null) {
+				System.out.println("<<CerbacPolicy Element Found>>");
+			}else{
+				return context.createFailureStatus();
+			}
 			
 		}
 		return context.createSuccessStatus();
